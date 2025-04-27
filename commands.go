89470dc244f0e76771b969b22f5c028f2d3a941f
K-Road <game@chrodeprime.com>
2025-04-27 17:31:37 +1000

    added unregister commands

--- commands.go
+++ commands.go
@@ -36,6 +36,11 @@ func commandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 }
 
 func registerCommands(dg *discordgo.Session) error {
+	err := unregisterCommands(dg)
+	if err != nil {
+		return err
+	}
+
 	commands := []*discordgo.ApplicationCommand{
 		{
 			Name:        "enable",
@@ -57,6 +62,26 @@ func registerCommands(dg *discordgo.Session) error {
 			return fmt.Errorf("cannot create '%v' command: %w", cmd.Name, err)
 		}
 		log.Printf("registered: %s", cmd.Name)
+	}
+
+	return nil
+}
+
+func unregisterCommands(dg *discordgo.Session) error {
+	// Get all commands registered for the bot
+	commands, err := dg.ApplicationCommands(dg.State.User.ID, "")
+	if err != nil {
+		return fmt.Errorf("failed to get registered commands: %w", err)
+	}
+
+	// Delete all commands
+	for _, cmd := range commands {
+		err := dg.ApplicationCommandDelete(dg.State.User.ID, "", cmd.ID)
+		if err != nil {
+			log.Printf("Failed to delete command %s: %v", cmd.Name, err)
+		} else {
+			log.Printf("Deleted command: %s", cmd.Name)
+		}
 	}
 
 	return nil
