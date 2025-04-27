cc10252980d6cb6aa23a964478d8ae02382e20ca
K-Road <game@chrodeprime.com>
2025-04-27 17:51:14 +1000

    removed debug test (mydiscord client needed a restart)

--- commands.go
+++ commands.go
@@ -48,7 +48,7 @@ func registerCommands(dg *discordgo.Session) error {
 		},
 		{
 			Name:        "disable",
-			Description: "Disable the bot test",
+			Description: "Disable the bot",
 		},
 		{
 			Name:        "moodweather",
